aff76d1c3d0d83373cbfa6aeb0a35dd0ce01708b
S.G <fomnen_s@etna-alternance.net>
2023-04-15 20:40:01 +0200

    [User] implementation CRUD

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,30 +1,74 @@
 import { Request, Response } from "express";
 // import User from "../database/models/User";
+import { Userinterface } from "../interfaces";
 
-let {
+const {
+  doc,
+  getDoc,
   collection,
   getDocs,
   addDoc,
 } = require("firebase/firestore/lite");
 
 const db = require('../database/instance');
+const bcrypt = require('bcrypt');
+
+// Get All Users
 
 export function getAll(req: Request, res: Response) {
   req = collection(db, "Users");
   getDocs(req).then((snapshoot: any) => {
-    let data = snapshoot.docs.map((doc: any) => doc.data());
-    res.json(data);
+    const QueryResult = snapshoot.docs.map((doc: any) => doc.data());
+    res.json(QueryResult);
   });
 }
 
-export const createUser = async (req: Request, res: Response) => {
+// Get by Id
+
+export async function getById(req: Request, res: Response) {
+
+  const user = req.params.id;
+  req = doc(db, "Users", user);
+
   try {
-    const data = collection(db, "Users");
-    addDoc(data, {
-      username: req.body.username,
-      email: req.body.email,
+    const QueryResult = await getDoc(req);
+    if(QueryResult.exists()) {
+      res.json(QueryResult.data());
+      console.log(QueryResult.data());
+    } else {
+      console.log('User does not exist')
+    }
+  } catch (error) {
+    res.send({
+      message: "This is an",
+      error,
     });
-    res.send("Record saved successfuly");
+  }
+}
+
+
+// Create a New User
+
+export const createUser = async (req: Request, res: Response) => {
+
+  const user: Userinterface = req.body;
+  const hash = await bcrypt.hash(user.password, 10);
+
+  try {
+    const QueryResult = collection(db, "Users");
+    addDoc(QueryResult, {
+      username: user.username,
+      email: user.email,
+      password : hash,
+      name : user.name,
+      age : user.age,
+      gender : user.gender,
+      location : user.location,
+      interests : user.interests,
+      profile_picture : user.profile_picture,
+    });
+    // res.json(QueryResult)
+    // res.send("Record saved successfuly");
   } catch (error) {
     res.send({
       message: "This is an",
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,12 @@
-import { createUser, getAll} from "../controllers/users.controller";
+import { createUser, getAll, getById} from "../controllers/users.controller";
 
 const express = require('express');
 
 const routeruser = express();
 
-routeruser.get('/users', getAll)
+routeruser.get('/users', getAll);
+
+routeruser.get('/users/:id', getById);
 
 routeruser.post('/users/register', createUser);
 
